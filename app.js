db241a30e43c9b3b1ef5029a60fa73cdf99dd01e
Per Djurner <per.djurner@gmail.com>
2018-04-15 14:48:51 +0200

    Use callback function for listening to port.

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ app.use(express.static("./public"))
 controller(app);
 
 //listen to port
-app.listen(3000)
-console.log("now listening to port 3000")
+app.listen(3000, function(){
+    console.log("NOW we're actually listening to port 3000. This is because app.listen is an asynchronous function, meaning that it provides a callback function that gets called when it has done its job (i.e. listening to port 3000 in this case). The callback function is the one we're in right now.");
+})
+console.log("now listening to port 3000 (not correct since this code executes before app.listen's callback function.)")
+// Check this for more info about async vs sync functions: https://www.youtube.com/watch?v=YxWMxJONp7E&list=PL4cUxeGkcC9jAhrjtZ9U93UMIhnCc44MH
